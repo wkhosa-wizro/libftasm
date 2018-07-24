1386b362319bd8a05a2625aeda1853f146f625aa
Wisani KHOSA <wkhosa@e4r11p6.wethinkcode.co.za>
2018-07-24 16:44:34 +0200

    update

--- test/test_puts.c
+++ test/test_puts.c
@@ -5,12 +5,8 @@ int		test_puts(void)
 	int		error;
 
 	error = 0;
-	printf("<");
 	ft_puts("");
-	printf(">\n");
-	printf("<");
 	ft_puts("This is a test string for puts!");
-	printf(">\n");
 
 	return (error);
 }
